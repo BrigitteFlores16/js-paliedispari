b7575a996577ae7ae16860eba212dfb2072ac6e2
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-10-18 21:52:04 +0200

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -11,4 +11,79 @@
 //*Dichiariamo chi ha vinto.
  
 
- 
+ //# procedimento
+
+//*Chiedere all’utente di inserire una parola
+let parolautente=prompt('inserisce una parola per verificare se è palindroma:');
+let parolainversa = [];
+for(let i = parolautente.length; i >= 0; i--)
+{
+    parolainversa.push(parolautente[i]);
+}
+parolainversa = parolainversa.join('');
+console.log("parola originale: " + parolautente + " parola inversa: " + parolainversa)
+if(parolainversa == parolautente)
+{
+    console.log("la parola è palindroma");
+}
+else
+{
+    console.log("la parola non è palindroma");
+}
+
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }  
+// se un numero è pari o dispari
+function isPari(numero) {
+    return numero % 2 == 0;
+}
+function gioco() {
+    let scelta = prompt("scegli se pari o dispari");
+    while (scelta != "dispari" && scelta != "pari")
+    {
+        console.log(scelta);
+        scelta = prompt("devi scrivere pari o dispari");
+    }
+    
+    let numeroutente = prompt("inserisci il numero che vuoi da 1 a 5");
+    let numerorandom = getRandomIntInclusive(1,5);
+    while (isNaN(numeroutente) || parseInt(numeroutente) < 1 || parseInt(numeroutente) > 5)
+        {
+            numeroutente = prompt("inserisci un numero valido da 1 a 5");
+        } 
+    numeroutente = parseInt(numeroutente);
+    let sommanumeri = numerorandom + numeroutente;
+    let ispari = isPari(sommanumeri);
+    let messaggio;
+    if(ispari == true)
+    {
+        messaggio = "la somma dei numeri è pari: " + sommanumeri 
+        if(scelta == "pari")
+        {
+            messaggio += " hai vinto!";
+        }
+        else
+        {
+            messaggio += " ha vinto il computer";
+        }
+    }
+    else
+    {
+        messaggio = "la somma dei numeri è dispari: " + sommanumeri 
+        if(scelta == "dispari")
+        {
+            messaggio += " hai vinto!";
+        }
+        else
+        {
+            messaggio += " ha vinto il computer";
+        }
+    }
+    alert(messaggio);
+}
+
+gioco();
